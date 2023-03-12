c73d38ef631b47c9e541fd0abe8bb30a506a50fa
choilin310 <choiinhwan0@gmail.com>
2023-03-12 13:17:01 -0400

    the arcade

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,53 +1,79 @@
-const cellElements = document.querySelectorAll('')
-let gameState = {
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null],
-    ],
-    player: 1,
-    // Maybe a current Player?
-    // A game status?? 'isPlaying' or 'over'
-    // Any other data your game logic depends on?
-  };
+let gameState = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+]
 
-  function renderGame() {
-    // Call this function after you've changed your state values
-    // Make references to DOM elements, and set the innerText,
-    // or innerHTML to reflect our gameState.board
-  }
+let player = 1;
+let  gameOver = false;
 
-  function switchPlayer() {
-    if (gameState.player === 1){
-        gameState.player = 2;
-    }else{
-        gameState.player = 1;
-    }
-  }
-  
-  function checkWin() {
-    for(let i = 0; i <= 2; i++){
-        if(board[i][i] === null){
-            continue;
-        }
-        if(gameState.board[i].every((cellRow) => gameState.board[i][0] === cellRow)){
-            return true;
-        }
-        if(gameState.board.map((cellList) => cellList[i]).every((cellColumn) => gameState.board[0][i] === cellColumn)){
-            return true;
-        }
-    }
-    if(gameState.board[0][0]===gameState.board[1][1] && gameState.board[1][1]===gameState.board[2][2] && gameState.board[0][0]===gameState.board[2][2]){
-        return true;
-    }
-    if(gameState.board[0][2]===gameState.board[1][1] && gameState.board[1][1]===gameState.board[2][0] && gameState.board[0][2]===gameState.board[2][0]){
-        return true;
-    }
-    return false;
-  }
+const cellElements = document.querySelectorAll(".cell")
 
-  board.addEventListener("click", function (event) {
-    // Figure out how to get the coordinates off event object (e.target.value)
-    // Use those coordinates to reference indexes in our gameState.board
-    // Set the position in our board to the current player
-  });
+//Event Listener
+cellElements.forEach((cell, index) => {
+  cell.addEventListener("click", () => {
+    placeMarker(index);
+  })
+}) 
+
+function placeMarker(index){
+  let col = index % 3
+  let row = (index - col) / 3
+    if(gameState[row][col] == 0 && gameOver == false){
+    gameState[row][col] = player;
+    player *= -1;
+    drawMarkers();
+    checkResults();
+  }
+}
+
+function drawMarkers(){
+  for (let row = 0; row < 3; row++){
+    for (let col = 0; col < 3; col++){
+      if (gameState [row][col] == 1){
+        cellElements[(row * 3) + col].classList.add("cross");
+      } else if(gameState[row][col] == -1){
+        cellElements[(row * 3) + col].classList.add("circle");
+      }
+    }
+  }
+}
+
+function checkResults(){
+  for (let i = 0; i < 3; i++){
+    let rowSum = gameState[i][0] +  gameState[i][1] + gameState[i][2];
+    let colSum = gameState[0 ][i] +  gameState[1][i] + gameState[2][i];
+    if (rowSum == 3 || colSum == 3){
+      endGame(1);
+      return
+    }else if(rowSum == -3 || colSum == -3){
+      endGame(2);
+      return
+    }
+  }
+  let diagonalSum1 = gameState[0][0] +  gameState[1][1] + gameState[2][2];
+  let diagonalSum2 = gameState[0][2] +  gameState[1][1] + gameState[2][0];
+  if (diagonalSum1 == 3 || diagonalSum2 == 3){
+    endGame(1);
+    return
+  }else if(diagonalSum1  == -3 || diagonalSum2 == -3){
+    endGame(2);
+    return
+  }
+  if (gameState[0].indexOf(0) == -1 &&
+      gameState[1].indexOf(0) == -1 &&
+      gameState[2].indexOf(0) == -1){
+        endGame(0);
+        return
+      }
+}
+
+function endGame(winner){
+  gameOver = true;
+  const resultElement = document.getElementById("result")
+  if(winner == 0){
+    resultElement.innerText = "It's a Tie" 
+  }else {
+    resultElement.innerText = `Player ${winner} wins!`
+  }
+}
